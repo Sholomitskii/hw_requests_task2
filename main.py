ec2c0b6f3d77a67ac7ca546a873e20b43abe68fa
Anton Sholomitskii <fskunk@yandex.ru>
2023-01-29 11:12:04 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import requests
+
+class YaUploader:
+    def __init__(self, token):
+        self.token = token
+
+    def get_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': 'OAuth {}'.format(self.token)
+        }
+    
+    def _get_upload_link(self, file_path):
+        upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        headers = self.get_headers()
+        params = {"path": file_path, "overwrite": "true"}
+        response = requests.get(upload_url, headers=headers, params=params)
+        return response.json()
+
+    def upload(self, file_path, filename):
+        href = self._get_upload_link(file_path=file_path).get("href")
+        response = requests.put(href, data=open(filename, 'rb'))
+        response.raise_for_status()
+        if response.status_code == 201:
+            print("Success")
+
+token = ''
+file_path = 'letters/letter.txt'
+filename = 'letter.txt'
+uploader = YaUploader(token)
+
+if __name__ == '__main__':
+    uploader.upload(file_path, filename)
